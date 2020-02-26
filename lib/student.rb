0e0f3c0993c0a4dd387b440a2905affd2d6ae56d
Robert M Ricci <rmricci121@gmail.com>
2020-02-26 19:56:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,12 +15,8 @@ class Student
   end
   
   def self.create_table
-    sql = <<- SQL 
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
-      grade TEXT)
-      SQL
+    sql = <<-  
+    
       
       DB[:conn].execute(sql)
   end
