6582c74b1b733eb728c811cc0368e9d5a255b159
Robert M Ricci <rmricci121@gmail.com>
2020-02-26 20:13:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,8 @@ class Student
   end
 end
   def update
-    sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.album, self.id)
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
