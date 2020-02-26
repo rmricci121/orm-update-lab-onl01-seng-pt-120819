dcea16f245605942c7455631fe736e8894e44e02
Robert M Ricci <rmricci121@gmail.com>
2020-02-26 19:39:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,15 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
+  def inititlaize(id, name, grade)
+    @id = id
+    @name = name 
+    @grade = grade
+    
+  end
+  
 
 end
