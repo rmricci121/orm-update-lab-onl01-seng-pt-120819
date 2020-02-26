9dec946ee85a7f28703eaa55cce1b45864abc422
Robert M Ricci <rmricci121@gmail.com>
2020-02-26 20:31:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,11 +8,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id 
   
-  def initialize(name, grade, id = nil)
-    @id = id
-    @name = name 
-    @grade = grade
-  end
+  # def initialize(name, grade, id = nil)
+  #   @id = id
+  #   @name = name 
+  #   @grade = grade
+  # end
   
   def self.create_table
     sql = <<-SQL
