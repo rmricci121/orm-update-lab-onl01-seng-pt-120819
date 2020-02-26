b79bd8d50a54dc8c8d65eb6b0645c23ad993ce8b
Robert M Ricci <rmricci121@gmail.com>
2020-02-26 20:06:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,14 @@ class Student
   end
   
   def save
-    
+    def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
   end
 
 end
