cd4eece1c993ff1f2159ff3fb9d2eefbab7608af
Robert M Ricci <rmricci121@gmail.com>
2020-02-26 20:20:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,8 +55,12 @@ end
     student 
   end
   
-  def self.new_from_db(array)
-    
+  def self.new_from_db(row)
+    new_student = self.new  
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student 
     
   end
 end
